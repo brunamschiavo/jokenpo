e0f358fa8186aa8641dd2caf83fd7d1ba2cf055d
Bruna Monteiro Schiavo <arqbrunaschiavo@gmail.com>
2025-08-06 22:41:10 -0300

    novos icones, novo fundo, novas regras

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const maoComputador = ["TESOURA", "PEDRA", "PAPEL", "TESOURA"];
+const maoComputador = [
+  "TESOURA",
+  "PEDRA",
+  "PAPEL",
+  "TESOURA",
+  "SPOCK",
+  "LIZARD",
+];
 
 // Adiconar cada item (pedra,papel, tesoura), adicionar um event listener em cada um.
 // A pedra vai imprimir: "jogou pedra" no console
@@ -14,9 +21,13 @@ const maoComputador = ["TESOURA", "PEDRA", "PAPEL", "TESOURA"];
 const pedra = document.getElementById("pedra");
 const papel = document.getElementById("papel");
 const tesoura = document.getElementById("tesoura");
+const spock = document.getElementById("spock");
+const lizard = document.getElementById("lizard");
 const pedraComputador = document.getElementById("pedraComputador");
 const papelComputador = document.getElementById("papelComputador");
 const tesouraComputador = document.getElementById("tesouraComputador");
+const spockComputador = document.getElementById("spockComputador");
+const lizardComputador = document.getElementById("lizardComputador");
 const modal = document.getElementById("modal");
 const playerScore = document.getElementById("playerScore");
 const computerScore = document.getElementById("computerScore");
@@ -31,16 +42,22 @@ const mostrarModal = (situacao) => {
 
 const jogar = (mao) => {
   console.log("JOGADOR JOGOU " + mao);
-  const aleatorio = maoComputador[Math.ceil(Math.random() * 3)];
+  const aleatorio = maoComputador[Math.ceil(Math.random() * 5)];
   if (aleatorio === "PEDRA") {
     console.log("COMPUTADOR JOGOU PEDRA");
-    pedraComputador.style.color = "oklch(81% 0.117 11.638)";
+    pedraComputador.style.color = "#B8DCD2";
   } else if (aleatorio === "PAPEL") {
     console.log("COMPUTADOR JOGOU PAPEL");
-    papelComputador.style.color = "oklch(81% 0.117 11.638)";
+    papelComputador.style.color = "#B8DCD2";
+  } else if (aleatorio === "SPOCK") {
+    console.log("COMPUTADOR JOGOU SPOCK");
+    spockComputador.style.color = "#B8DCD2";
+  } else if (aleatorio === "LIZARD") {
+    console.log("COMPUTADOR JOGOU LIZARD");
+    lizardComputador.style.color = "#B8DCD2";
   } else {
     console.log("COMPUTADOR JOGOU TESOURA");
-    tesouraComputador.style.color = "oklch(81% 0.117 11.638)";
+    tesouraComputador.style.color = "#B8DCD2";
   }
 
   if (mao === aleatorio) {
@@ -51,48 +68,137 @@ const jogar = (mao) => {
   } else if (mao === "PAPEL" && aleatorio === "PEDRA") {
     mostrarModal("GANHOU");
     playerScore.innerText++;
-  } else if (mao === "TESOURA" && aleatorio === "PAPEL") {
+  } else if (mao === "PEDRA" && aleatorio === "LIZARD") {
+    mostrarModal("GANHOU");
+    playerScore.innerText++;
+  } else if (mao === "LIZARD" && aleatorio === "SPOCK") {
+    mostrarModal("GANHOU");
+  } else if (mao === "SPOCK" && aleatorio === "TESOURA") {
+    mostrarModal("GANHOU");
+  } else if (mao === "TESOURA" && aleatorio === "LIZARD") {
+    mostrarModal("GANHOU");
+  } else if (mao === "LAGARTO" && aleatorio === "PAPEL") {
+    mostrarModal("GANHOU");
+  } else if (mao === "PAPEL" && aleatorio === "SPOCK") {
+    mostrarModal("GANHOU");
+  } else if (mao === "SPOCK" && aleatorio === "PEDRA") {
     mostrarModal("GANHOU");
     playerScore.innerText++;
   } else {
     mostrarModal("PERDEU");
     computerScore.innerText++;
   }
+  if (computerScore.innerText === "5" || playerScore.innerText === "5") {
+    playerScore.innerText = "0";
+    computerScore.innerText = "0";
+  }
 
   setTimeout(() => {
-    pedra.style.color = "#8596a6";
-    papel.style.color = "#8596a6";
-    tesoura.style.color = "#8596a6";
-    pedraComputador.style.color = "#8596a6";
-    papelComputador.style.color = "#8596a6";
-    tesouraComputador.style.color = "#8596a6";
+    pedra.style.color = "#fd9860";
+    papel.style.color = "#fd9860";
+    tesoura.style.color = "#fd9860";
+    spock.style.color = "#fd9860";
+    lizard.style.color = "#fd9860";
+    pedraComputador.style.color = "#f0cc51";
+    papelComputador.style.color = "#f0cc51";
+    tesouraComputador.style.color = "#f0cc51";
+    spockComputador.style.color = "#f0cc51";
+    lizardComputador.style.color = "#f0cc51";
 
     pedra.disabled = false;
     papel.disabled = false;
     tesoura.disabled = false;
+    spock.disabled = false;
+    lizard.disabled = false;
   }, 1000);
 };
 
 pedra.addEventListener("click", () => {
-  pedra.style.color = "oklch(81% 0.117 11.638)";
+  pedra.style.color = "#B8DCD2";
   pedra.disabled = true;
   papel.disabled = true;
   tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
   jogar("PEDRA");
 });
 
 papel.addEventListener("click", () => {
-  papel.style.color = "oklch(81% 0.117 11.638)";
+  papel.style.color = "#B8DCD2";
   pedra.disabled = true;
   papel.disabled = true;
   tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
   jogar("PAPEL");
 });
 
 tesoura.addEventListener("click", () => {
-  tesoura.style.color = "oklch(81% 0.117 11.638)";
+  tesoura.style.color = "#B8DCD2";
   pedra.disabled = true;
   papel.disabled = true;
   tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
   jogar("TESOURA");
+});
+
+spock.addEventListener("click", () => {
+  spock.style.color = "#B8DCD2";
+  pedra.disabled = true;
+  papel.disabled = true;
+  tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
+  jogar("SPOCK");
+});
+
+lizard.addEventListener("click", () => {
+  lizard.style.color = "#B8DCD2";
+  pedra.disabled = true;
+  papel.disabled = true;
+  tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
+  jogar("LAGARTO");
+});
+
+papelComputador.addEventListener("click", () => {
+  papel.style.color = "#B8DCD2";
+  pedra.disabled = true;
+  papel.disabled = true;
+  tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
+  jogar("PAPEL");
+});
+
+tesouraComputador.addEventListener("click", () => {
+  tesoura.style.color = "#B8DCD2";
+  pedra.disabled = true;
+  papel.disabled = true;
+  tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
+  jogar("TESOURA");
+});
+
+spockComputador.addEventListener("click", () => {
+  tesoura.style.color = "#B8DCD2";
+  pedra.disabled = true;
+  papel.disabled = true;
+  tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
+  jogar("SPOCK");
+});
+
+lizardComputador.addEventListener("click", () => {
+  tesoura.style.color = "#B8DCD2";
+  pedra.disabled = true;
+  papel.disabled = true;
+  tesoura.disabled = true;
+  spock.disabled = true;
+  lizard.disabled = true;
+  jogar("LAGARTO");
 });
